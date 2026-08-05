3a27f57dc465e46440d24a5d7641f34ff69fa915
isseat8-hash <isseat8@gmail.com>
2026-08-05 17:08:54 +0300

    Update logs.js

--- systems/logs.js
+++ systems/logs.js
@@ -1,1 +1,45 @@
+const {
+EmbedBuilder
+}=require("discord.js");
 
+
+module.exports=
+async(guild,title,text)=>{
+
+
+const channel=
+guild.channels.cache.find(
+x=>x.name==="logs"
+);
+
+
+if(!channel)return;
+
+
+
+const embed=
+new EmbedBuilder()
+
+.setColor("#00BFFF")
+
+.setTitle(
+"⚔️ AEGİS NW LOG"
+)
+
+.addFields({
+
+name:title,
+value:text
+
+})
+
+.setTimestamp();
+
+
+
+channel.send({
+embeds:[embed]
+});
+
+
+}
