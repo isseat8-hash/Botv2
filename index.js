1bd1821e31e0e9eb9326afc3e719271f91aa6750
isseat8-hash <isseat8@gmail.com>
2026-08-05 17:07:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
+const {
+Client,
+GatewayIntentBits,
+Collection
+} = require("discord.js");
 
+const config = require("./config.json");
+
+const client = new Client({
+
+intents:[
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent,
+GatewayIntentBits.GuildMembers
+]
+
+});
+
+
+client.commands = new Collection();
+
+
+
+client.on("ready",()=>{
+
+console.log(
+`⚔️ AEGİS NW Aktif | ${client.user.tag}`
+);
+
+client.user.setActivity(
+"AEGİS NW Network",
+{
+type:3
+});
+
+});
+
+
+
+// KOMUTLAR
+
+client.on("messageCreate",async message=>{
+
+
+if(message.author.bot) return;
+
+
+if(message.content === "!ip"){
+
+require("./commands/ip")
+(message);
+
+}
+
+
+
+if(message.content === "!panel"){
+
+require("./commands/panel")
+(message);
+
+}
+
+
+});
+
+
+
+
+// BUTONLAR
+
+client.on(
+"interactionCreate",
+async interaction=>{
+
+
+if(interaction.isButton()){
+
+require("./interactions/buttons")
+(interaction);
+
+}
+
+
+
+if(interaction.isModalSubmit()){
+
+require("./interactions/modals")
+(interaction);
+
+}
+
+
+});
+
+
+client.login(config.token);
