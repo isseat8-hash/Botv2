8088936f70b4646ccfff00e141d7a7b21a109dbd
isseat8-hash <isseat8@gmail.com>
2026-08-05 17:11:30 +0300

    Update buttons.js

--- interactions/buttons.js
+++ interactions/buttons.js
@@ -1,1 +1,60 @@
+const {
+PermissionsBitField
+}=require("discord.js");
 
+
+module.exports = async interaction=>{
+
+
+if(
+interaction.customId==="ticket_close"
+){
+
+
+if(
+!interaction.member.roles.cache.has(
+require("../config.json").ticketSupportRole
+)
+&&
+!interaction.member.permissions.has(
+PermissionsBitField.Flags.Administrator
+)
+){
+
+return interaction.reply({
+
+content:
+"鉂� Sadece destek ekibi ticket kapatabilir.",
+
+ephemeral:true
+
+});
+
+}
+
+
+
+await interaction.reply({
+
+content:
+"馃敀 Ticket 5 saniye i莽inde kapat谋lacak.",
+
+ephemeral:true
+
+});
+
+
+
+setTimeout(()=>{
+
+interaction.channel.delete();
+
+},5000);
+
+
+
+}
+
+
+
+};
